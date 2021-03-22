8be2ec1d8f7307f5f7e9d3b5f02cbd84d0ca759b
toron2c <toron2c@yandex.ru>
2021-03-22 15:50:03 +0300

    lesson3

--- scripts/app4.js
+++ scripts/app4.js
@@ -1,41 +1,36 @@
-"use strict";
-// 4-e задание.
-/**
- * Функция сложения, складывает параметры
- */
-const sum = (a, b) => a + b;
-/**
- * Функция вычитания, вычитает из параметра a, параметр b
- */
-const deduct = (a, b) => a - b;
-/**
- * Функция умножения, умножает параметры
- */
-const multiply = (a, b) => a * b;
-/**
- * Функция деления , делит параметр a на параметр b
- */
-const divide = (a, b) => a / b;
+'use strict';
 
-//5-е задание.
-/**
- * Функция определяет математическую операцию и вызывает функцию которая отвечает за данную операцию,передавая в неё значения аргументов, и возвращает результат.
- * @param {Number} arg1 
- * @param {Number} arg2 
- * @param {String} operation 
- * @returns {Number} возвращает полученный результат
- */
-const mathOperation = (arg1, arg2, operation) => {
-    switch (operation) {
-        case "сложение":
-            return sum(arg1, arg2);
-        case "вычитание":
-            return deduct(arg1, arg2);
-        case "умножение":
-            return multiply(arg1, arg2);
-        case "деление":
-            return divide(arg1, arg2);
-        default:
-            return "Такой операции не предусмотренно";
-    }
-}
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: []
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+//1. Получить массив со всеми продуктами у которых есть фотографии
+let newArray = products.filter(product => "photos" in product && product.photos != 0);
+//2.Выводим новый массив
+console.log(newArray);
+//3.Отсортировать товары по цене,от низкой к высокой.
+products.sort((a, b) => a.price > b.price ? 1 : -1);
+//выводим на консоль отсортированый массив
+console.log(products);
---
+++ scripts/app6.js
@@ -0,0 +1,5 @@
+'use strict';
+// Задание 6. 
+for (let i = 'x'; i.length <= 25.; i += 'x') {
+    console.log(i);
+}
